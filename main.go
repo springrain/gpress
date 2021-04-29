736e696ebc874ec4c7c0681d14a1f43d5b8330c2
springrain <chunanyong@163.com>
2021-04-29 20:08:32 +0800

    增加模板调用,自定义标签

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"html/template"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	//设置自定义i函数.要在LoadHTMLGlob加载模板之前.
+	router.SetFuncMap(template.FuncMap{"md5": MD5})
+
+	router.GET("/", IndexApi)
+	//LoadHTMLFiles(templates...)
+	//router.LoadHTMLGlob("templates/**/*")
+	router.LoadHTMLGlob("templates/*")
+
+	router.Run(":8080")
+}
+
+//处理函数
+func IndexApi(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{"name": "test"})
+	return
+}
+
+//自定义函数
+func MD5(in string) (string, error) {
+	hash := md5.Sum([]byte(in))
+	return hex.EncodeToString(hash[:]), nil
 }
