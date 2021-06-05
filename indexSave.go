807b3bb6830a5e54d2ad9ef62fc245d5c53589d3
springrain <chunanyong@163.com>
2021-06-05 17:39:44 +0800

    update tip ： 通用save方法

---
+++ indexSave.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func saveNexIndex(newIndex map[string]interface{}, tableName string) (map[string]string, error) {
+
+	SearchResult, err := findIndexFields(tableName, 1)
+	m := make(map[string]string, 2)
+
+	if err != nil {
+		FuncLogError(err)
+		m["code"] = "303"
+		m["msg"] = "查询异常"
+		return m, err
+	}
+	id := FuncGenerateStringID()
+	newIndex["id"] = id
+	result := SearchResult.Hits
+
+	for _, v := range result {
+		tmp := fmt.Sprintf("%v", v.Fields["FieldCode"]) //转为字符串
+		_, ok := newIndex[tmp]
+		if ok {
+			if newIndex[tmp] == nil || fmt.Sprintf("%v", newIndex[tmp]) == "" {
+				m["code"] = "401"
+				m["msg"] = tmp + "不能为空"
+				return m, nil
+			}
+			m["code"] = "401"
+			m["msg"] = tmp + "不能为空"
+			return m, nil
+		}
+
+	}
+	IndexMap[tableName].Index(id, newIndex)
+
+	m["code"] = "200"
+	m["msg"] = "保存成功"
+	return m, nil
+
+}
