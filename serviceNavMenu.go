392ae5a56eca35b233f7335e9555b7a762ef8d41
springrain <chunanyong@163.com>
2021-06-10 12:13:51 +0800

    增加分页的处理

--- serviceNavMenu.go
+++ serviceNavMenu.go
@@ -5,52 +5,7 @@ import (
 	"fmt"
 
 	"github.com/blevesearch/bleve/v2"
-	"github.com/blevesearch/bleve/v2/search/query"
 )
-
-//获取表中符合条件字段
-//indexName: 表名
-//isRequired: 是否可以为空
-//打开文件
-var inclusive = true
-
-func findIndexFields(indexName string, isRequired int) (result *bleve.SearchResult, err error) {
-	if err != nil {
-		FuncLogError(err)
-		return nil, err
-	}
-
-	var query *query.ConjunctionQuery
-	var index = IndexMap[indexFieldIndexName]
-	// 查询指定表
-	queryIndexCode := bleve.NewTermQuery(indexName)
-	//查询指定字段
-	queryIndexCode.SetField("IndexCode")
-	if isRequired != 1 && isRequired != 0 {
-		query = bleve.NewConjunctionQuery(queryIndexCode)
-
-	} else {
-
-		var f float64 = float64(isRequired)
-		queryIsReqired := bleve.NewNumericRangeInclusiveQuery(&f, &f, &inclusive, &inclusive)
-		queryIsReqired.SetField("Required")
-		query = bleve.NewConjunctionQuery(queryIndexCode, queryIsReqired)
-	}
-
-	//query: 条件  size:大小  from :起始
-	serarch := bleve.NewSearchRequestOptions(query, 1000, 0, false)
-	//查询所有字段
-	serarch.Fields = []string{"*"}
-
-	result, err = index.SearchInContext(context.Background(), serarch)
-
-	if err != nil {
-		FuncLogError(err)
-		return nil, err
-	}
-
-	return result, nil
-}
 
 //获取菜单树 pid 为0 为一级菜单
 var active float64 = 1
