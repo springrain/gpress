c8c3b1b9be2d5fe2252bf3ae92b10ff0832365b1
springrain <chunanyong@163.com>
2023-01-07 20:17:32 +0800

    处理admin登录拦截

--- routeAdmin.go
+++ routeAdmin.go
@@ -13,6 +13,9 @@ import (
 	"github.com/cloudwego/hertz/pkg/protocol"
 )
 
+// 模板路径,正常应该从siteInfo里获取,这里用于演示
+var themePath = "/theme/default/"
+
 func initAdminRoute() {
 	// 默认首页
 	h.GET("/", funcIndex)
@@ -109,35 +112,28 @@ func initAdminRoute() {
 		result, _ := getNavMenu("0")
 		c.JSON(http.StatusOK, result)
 	})
+
 	// 后台管理员登录
 	h.GET("/admin/login", func(ctx context.Context, c *app.RequestContext) {
-		c.HTML(http.StatusOK, "admin/login.html", nil)
+		c.HTML(http.StatusOK, "/admin/login.html", nil)
 	})
 	h.POST("/admin/login", func(ctx context.Context, c *app.RequestContext) {
 		userName := c.PostForm("userName")
-		password := c.PostForm("password")
-
 		jwttoken, _ := newJWTToken(userName, nil)
-
-		fmt.Printf("jwttoken:%s,password:%s", jwttoken, password)
 
 		//c.HTML(http.StatusOK, "admin/index.html", nil)
 		c.SetCookie(jwttokenKey, jwttoken, timeout, "/", "", protocol.CookieSameSiteStrictMode, true, true)
 
 		c.Redirect(http.StatusOK, []byte("/admin/index"))
 	})
-
+	//admin路由组
+	admin := h.Group("/admin")
+	admin.Use(adminHandler())
 	// 后台管理员首页
-	h.GET("/admin/index", func(ctx context.Context, c *app.RequestContext) {
-		jwttoken := c.Cookie(jwttokenKey)
-		userName, err := userIdByToken(string(jwttoken))
-		if err != nil {
-			c.HTML(http.StatusOK, "admin/error.html", nil)
-			return
-		}
-		fmt.Println(userName)
-		//fmt.Println(c.Request.Body())
-		c.HTML(http.StatusOK, "admin/index.html", nil)
+	admin.GET("/index", func(ctx context.Context, c *app.RequestContext) {
+
+		fmt.Println(c.Get(tokenUserId))
+		c.HTML(http.StatusOK, "/admin/index.html", nil)
 	})
 
 }
@@ -155,4 +151,18 @@ func funcMD5(in string) ([]string, error) {
 	list[0] = in
 	list[1] = hex.EncodeToString(hash[:])
 	return list, nil
+}
+
+// adminHandler admin权限拦截器
+func adminHandler() app.HandlerFunc {
+	return func(ctx context.Context, c *app.RequestContext) {
+		jwttoken := c.Cookie(jwttokenKey)
+		userId, err := userIdByToken(string(jwttoken))
+		if err != nil || userId == "" {
+			c.Redirect(http.StatusOK, []byte("/admin/login"))
+			c.Abort() //终止后续调用
+			return
+		}
+		c.Set(tokenUserId, userId)
+	}
 }
