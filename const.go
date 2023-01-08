67140500907bf4bdafcd370afe6bba8cf5ed1e89
springrain <chunanyong@163.com>
2023-01-08 20:56:47 +0800

    安装成功后,将config配置写入到索引

--- const.go
+++ const.go
@@ -8,21 +8,21 @@ const (
 	// 数据目录,如果不存在认为是第一次安装启动,会创建默认的数据
 	bleveDataDir = datadir + "bleve/"
 	// 索引字段的名称
-	indexFieldIndexName = bleveDataDir + "IndexField"
+	indexFieldIndexName = bleveDataDir + "indexField"
 	// User 用户的索引名称
-	userIndexName = bleveDataDir + "User"
+	userIndexName = bleveDataDir + "user"
 	// siteInfo  站点信息
 	indexSitenIndexName = bleveDataDir + "sitenInfo"
 	// 页面模板
 	indexPageTemplateName = bleveDataDir + "pageTemplate"
 	// 导航菜单
-	indexNavMenuName = bleveDataDir + "NavMenu"
+	indexNavMenuName = bleveDataDir + "navMenu"
 	// 模型
-	indexModuleName = bleveDataDir + "Module"
+	indexModuleName = bleveDataDir + "module"
 	// 模型数据
 	indexModuleDefaultName = "moduleDefault"
 	// 文章内容
-	indexContentName = bleveDataDir + "Content"
+	indexContentName = bleveDataDir + "content"
 	//---------------------------//
 
 	//模板的路径
