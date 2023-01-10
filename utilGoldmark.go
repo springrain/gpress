4ccd6185352b8efb930054e083c65612aca73055
springrain <chunanyong@163.com>
2023-01-10 21:19:05 +0800

    使用goldmark官方meta插件

--- utilGoldmark.go
+++ utilGoldmark.go
@@ -5,27 +5,34 @@ import (
 	"os"
 
 	"github.com/yuin/goldmark"
+	meta "github.com/yuin/goldmark-meta"
 	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
 )
 
-var markdown goldmark.Markdown
+var md goldmark.Markdown
 
 func init() {
-	markdown = goldmark.New(
-		goldmark.WithExtensions(extension.GFM, extension.Footnote),
+	md = goldmark.New(
+		goldmark.WithExtensions(extension.GFM,
+			extension.Footnote,
+			meta.Meta,
+		),
 	)
 }
 
-func conver2Html(mkfile string) (*string, error) {
+func conver2Html(mkfile string) (map[string]interface{}, *string, error) {
 	source, err := os.ReadFile(mkfile)
 	html := ""
 	if err != nil {
-		return &html, err
+		return nil, &html, err
 	}
-	var buf bytes.Buffer
-	if err := markdown.Convert(source, &buf); err != nil {
-		return &html, err
+	var htmlBuffer bytes.Buffer
+	context := parser.NewContext()
+	if err := md.Convert([]byte(source), &htmlBuffer, parser.WithContext(context)); err != nil {
+		return nil, &html, err
 	}
-	html = buf.String()
-	return &html, err
+	metaData := meta.Get(context)
+	html = htmlBuffer.String()
+	return metaData, &html, err
 }
