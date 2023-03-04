1a2ea14887d362565da5745189e3f6e996a7092e
springrain <chunanyong@163.com>
2023-03-04 20:52:07 +0800

    更新mod依赖,页面测试函数

---
+++ hugoFunc.go
@@ -0,0 +1,20 @@
+package main
+
+import "html/template"
+
+// funcT 多语言i18n适配,例如 {{ T "nextPage" }}
+func funcT(key string) (string, error) {
+
+	return key, nil
+}
+
+// funcSafeHTML 转义html字符串
+func funcSafeHTML(html string) (string, error) {
+	ss := template.HTMLEscapeString(html)
+	return ss, nil
+}
+
+// funcRelURL 真实的url
+func funcRelURL(url string) (string, error) {
+	return funcSafeHTML(url)
+}
