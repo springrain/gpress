59c20e2d0fac62f1112c5f43f34cddb29c203fc7
springrain <chunanyong@163.com>
2023-03-21 15:09:33 +0800

    修改config存储格式,全部使用map保存,避免对象操作

--- initConfig.go
+++ initConfig.go
@@ -30,13 +30,13 @@ func initConfig() (bool, error) {
 	}
 	indexField.Index(configId.ID, configId)
 
-	// 配置表的 configKey 字段
-	configKey := IndexFieldStruct{
+	// 配置 basePath 字段
+	basePath := IndexFieldStruct{
 		ID:           FuncGenerateStringID(),
 		IndexCode:    configIndexName,
-		IndexName:    "配置的configKey",
-		FieldCode:    "configKey",
-		FieldName:    "编码",
+		IndexName:    "配置信息",
+		FieldCode:    "basePath",
+		FieldName:    "基础路径",
 		FieldType:    3,
 		AnalyzerName: keyword.Name,
 		CreateTime:   now,
@@ -44,22 +44,87 @@ func initConfig() (bool, error) {
 		SortNo:       2,
 		Active:       1,
 	}
-	indexField.Index(configKey.ID, configKey)
-	// 用户表的 configValue 字段
-	configValue := IndexFieldStruct{
+	indexField.Index(basePath.ID, basePath)
+
+	// 配置 jwtSecret 字段
+	jwtSecret := IndexFieldStruct{
 		ID:           FuncGenerateStringID(),
 		IndexCode:    configIndexName,
-		IndexName:    "用户信息",
-		FieldCode:    "configValue",
-		FieldName:    "用户名称",
+		IndexName:    "配置信息",
+		FieldCode:    "jwtSecret",
+		FieldName:    "jwt加密字符串",
 		FieldType:    3,
 		AnalyzerName: keyword.Name,
 		CreateTime:   now,
 		CreateUser:   createUser,
-		SortNo:       3,
+		SortNo:       4,
 		Active:       1,
 	}
-	indexField.Index(configValue.ID, configValue)
+	indexField.Index(jwtSecret.ID, jwtSecret)
+
+	// 配置 jwtSecret 字段
+	jwttokenKey := IndexFieldStruct{
+		ID:           FuncGenerateStringID(),
+		IndexCode:    configIndexName,
+		IndexName:    "配置信息",
+		FieldCode:    "jwttokenKey",
+		FieldName:    "jwt的key",
+		FieldType:    3,
+		AnalyzerName: keyword.Name,
+		CreateTime:   now,
+		CreateUser:   createUser,
+		SortNo:       5,
+		Active:       1,
+	}
+	indexField.Index(jwttokenKey.ID, jwttokenKey)
+
+	// 配置 serverPort 字段
+	serverPort := IndexFieldStruct{
+		ID:           FuncGenerateStringID(),
+		IndexCode:    configIndexName,
+		IndexName:    "配置信息",
+		FieldCode:    "serverPort",
+		FieldName:    "服务器ip:端口",
+		FieldType:    3,
+		AnalyzerName: keyword.Name,
+		CreateTime:   now,
+		CreateUser:   createUser,
+		SortNo:       6,
+		Active:       1,
+	}
+	indexField.Index(serverPort.ID, serverPort)
+
+	// 配置 theme 字段
+	theme := IndexFieldStruct{
+		ID:           FuncGenerateStringID(),
+		IndexCode:    configIndexName,
+		IndexName:    "配置信息",
+		FieldCode:    "theme",
+		FieldName:    "主题",
+		FieldType:    3,
+		AnalyzerName: keyword.Name,
+		CreateTime:   now,
+		CreateUser:   createUser,
+		SortNo:       7,
+		Active:       1,
+	}
+	indexField.Index(theme.ID, theme)
+
+	// 配置 timeout 字段
+	timeout := IndexFieldStruct{
+		ID:           FuncGenerateStringID(),
+		IndexCode:    configIndexName,
+		IndexName:    "配置信息",
+		FieldCode:    "timeout",
+		FieldName:    "超时时间",
+		FieldType:    1,
+		AnalyzerName: keyword.Name,
+		CreateTime:   now,
+		CreateUser:   createUser,
+		SortNo:       8,
+		Active:       1,
+	}
+	indexField.Index(timeout.ID, timeout)
 
 	// 创建配置表的索引
 	mapping := bleve.NewIndexMapping()
