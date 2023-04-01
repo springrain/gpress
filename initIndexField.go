bfa365bd1a932984936b6554c88be66634dfba85
springrain <chunanyong@163.com>
2023-04-01 15:47:47 +0800

    暂时使用keyword,优先保证可以精确查询

--- initIndexField.go
+++ initIndexField.go
@@ -53,8 +53,8 @@ func initIndexField() (bool, error) {
 	}
 	mapping := bleve.NewIndexMapping()
 	// 指定默认的分词器,存在问题:NewQueryStringQuery时不能正确匹配查询
-	//mapping.DefaultMapping.DefaultAnalyzer = keywordAnalyzerName
-	mapping.DefaultMapping.AddFieldMappingsAt("*", keywordMapping)
+	mapping.DefaultMapping.DefaultAnalyzer = keywordAnalyzerName
+	// mapping.DefaultMapping.AddFieldMappingsAt("*", keywordMapping)
 	index, err := bleve.New(indexFieldName, mapping)
 	if err != nil {
 		FuncLogError(err)
