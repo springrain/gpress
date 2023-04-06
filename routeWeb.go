193052b11ef5dd98175f940ab9977e135ac8e363
springrain <chunanyong@163.com>
2023-04-06 14:45:57 +0800

    修改内容渲染

--- routeWeb.go
+++ routeWeb.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/cloudwego/hertz/pkg/app"
 )
@@ -17,6 +19,12 @@ func init() {
 
 // funcIndex 模板首页
 func funcIndex(ctx context.Context, c *app.RequestContext) {
+	pageNoStr := c.DefaultQuery("pageNo", "1")
+	pageNo, _ := strconv.Atoi(pageNoStr)
+	q := strings.TrimSpace(c.Query("q"))
+
 	data := make(map[string]interface{}, 0)
+	data["pageNo"] = pageNo
+	data["q"] = q
 	c.HTML(http.StatusOK, "index.html", data)
 }
