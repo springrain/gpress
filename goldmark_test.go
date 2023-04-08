f54bb62a97bfcdd2c7e995453413b9ddc33ac3c1
springrain <chunanyong@163.com>
2023-04-08 17:29:34 +0800

    增加迁移的测试用例

--- goldmark_test.go
+++ goldmark_test.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
 func TestGoldmarkMeta(t *testing.T) {
-	metaData, tocHtml, html, _ := conver2Html(datadir + "post/01-nginx-config.md")
+	source, err := os.ReadFile(datadir + "post/34-es-config.md")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	metaData, tocHtml, html, _ := conver2Html(source)
+	fmt.Println(metaData["categories"])
+	fmt.Println(metaData["tags"])
 	fmt.Println(metaData)
 	fmt.Println(*tocHtml)
 	fmt.Println(*html)
