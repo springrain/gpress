ac1a822c9bd82d1bf804b4d739a4a7bf607a2d58
springrain <chunanyong@163.com>
2023-05-11 16:58:50 +0800

    使用jsonld原生实现activitypub协议,不再依赖go-ap/activitypub

--- utilActivityPub.go
+++ utilActivityPub.go
@@ -1,12 +1,6 @@
 package main
 
-import (
-	"github.com/go-ap/activitypub"
-)
-
 func createAPObject() error {
-	apObject := activitypub.ObjectNew(activitypub.ArticleType)
-	apObject.ID = activitypub.ID("gpress")
 
 	return nil
 
