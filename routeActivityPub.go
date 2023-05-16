a942985f7090055944bd085327c86abca5e1e50e
springrain <chunanyong@163.com>
2023-05-16 11:39:02 +0800

    修改 ap响应的路径

--- routeActivityPub.go
+++ routeActivityPub.go
@@ -2,5 +2,6 @@ package main
 
 func init() {
 	h.GET("/.well-known/webfinger", funcWebFinger)
-	h.GET("/api/acititypub/users/:userName", funcActivityPubUserInfo)
+	h.GET("/acititypub/api/user/:userName", funcActivityPubUsers)
+	h.GET("/acititypub/api/outbox/:userName", funcActivityPubOutBox)
 }
