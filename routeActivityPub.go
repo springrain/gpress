e0f1a7ed2d7596c996df405598c97dc4b08ff427
springrain <chunanyong@163.com>
2023-05-17 16:37:28 +0800

    修改接口签名验证

--- routeActivityPub.go
+++ routeActivityPub.go
@@ -1,8 +1,21 @@
 package main
 
 func init() {
+	//resource根信息查询
 	h.GET("/.well-known/webfinger", funcWebFinger)
+
+	//用户信息查询
 	h.GET("/activitypub/api/user/:userName", funcActivityPubUsers)
+	//outbox信息查询
 	h.GET("/activitypub/api/outbox/:userName", funcActivityPubOutBox)
 	h.GET("/activitypub/api/outbox_page/:userName/:pageNo", funcActivityPubOutBoxPage)
+
+	//inbox信息查询
+	h.POST("/activitypub/api/inbox/:userName", activitySignatureHandler, funcActivityPubInBox)
+
+	//关注
+	h.GET("/activitypub/api/following/:userName", funcActivityPubOutBox)
+	//粉丝
+	h.GET("/activitypub/api/followers/:userName", funcActivityPubOutBox)
+
 }
