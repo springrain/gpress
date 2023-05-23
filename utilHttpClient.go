98f671a31df1679ee6be04bf18b59ea95bdf2ecf
springrain <chunanyong@163.com>
2023-05-23 11:02:21 +0800

    分离header,有前端钱包进行签名传递

--- utilHttpClient.go
+++ utilHttpClient.go
@@ -19,15 +19,15 @@ const (
 	activityPubAccept      = "application/activity+json"
 	activityPubContentType = "application/activity+json; charset=utf-8"
 	//activityPubDefaultDomain = "activitypub.gpress.cn"
-	activityPubDefaultDomain = "activitypub.gpress.cn"
+	activityPubDefaultDomain = "testmakerone.shengjian.net"
 	proxyAddress             = "http://127.0.0.1:49864/"
 	//keyId                    = "https://" + activityPubDefaultDomain + "/activitypub/api/user/test11#main-key"
 )
 
 // responseJsonValue 发送GET请求,把返回值的json获取指定的key,例如
 func responseJsonValue(httpurl string, jsonKey string, keyId string) (interface{}, error) {
-
-	bodyMap, err := sendActivityPubRequest(httpurl, consts.MethodGet, nil, keyId, false)
+	headerMap, _ := wrapRequestHeader(httpurl, consts.MethodGet, nil, keyId, false)
+	bodyMap, err := sendActivityPubRequest(httpurl, consts.MethodGet, nil, headerMap)
 	if err != nil {
 		return nil, err
 	}
@@ -51,8 +51,8 @@ func responseJsonValue(httpurl string, jsonKey string, keyId string) (interface{
 	return value, nil
 }
 
-// sendActivityPubRequest 返送activityPub
-func sendActivityPubRequest(httpurl string, method string, body map[string]interface{}, keyId string, isSign bool) (map[string]interface{}, error) {
+// sendActivityPubRequest 发送activityPub请求
+func sendActivityPubRequest(httpurl string, method string, body map[string]interface{}, headerMap map[string]string) (map[string]interface{}, error) {
 	// 解析 URL 字符串
 	parsedURL, err := url.Parse(httpurl)
 	if err != nil {
@@ -71,15 +71,10 @@ func sendActivityPubRequest(httpurl string, method string, body map[string]inter
 		c.SetProxy(protocol.ProxyURI(protocol.ParseURI(proxyAddress)))
 	}
 
-	// mastodon 的日期格式为RFC2616 -- time.RFC1123
-	date := time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05 GMT")
-
 	bodyByte := []byte("")
 	if body != nil {
 		bodyByte, _ = json.Marshal(body)
 	}
-	hash := sha256.Sum256(bodyByte)
-	digest := "SHA-256=" + base64.StdEncoding.EncodeToString(hash[:])
 
 	request := &protocol.Request{}
 	request.SetRequestURI(httpurl)
@@ -88,29 +83,11 @@ func sendActivityPubRequest(httpurl string, method string, body map[string]inter
 	request.Header.SetContentTypeBytes([]byte(activityPubContentType))
 	request.SetHeader("Accept", activityPubAccept)
 	request.SetHeader("Host", host)
-	request.SetHeader("Digest", digest)
-	request.SetHeader("Date", date)
 
-	if isSign {
-		headers := "(request-target) host date digest content-type"
-		comparisonStrings := make([]string, 0)
-		comparisonStrings = append(comparisonStrings, "(request-target): "+strings.ToLower(method)+" "+parsedURL.Path)
-		comparisonStrings = append(comparisonStrings, "host: "+host)
-		comparisonStrings = append(comparisonStrings, "date: "+date)
-		comparisonStrings = append(comparisonStrings, "digest: "+digest)
-		comparisonStrings = append(comparisonStrings, "content-type: "+activityPubContentType)
-		signatureString := strings.Join(comparisonStrings, "\n")
-
-		//生成签名数据
-		signatureBase64, err := generateRSASignature(signatureString)
-		if err != nil {
-			return nil, err
-		}
-		// 构建 signature
-		signature := fmt.Sprintf(`keyId="%s",algorithm="rsa-sha256",headers="%s",signature="%s"`, keyId, headers, signatureBase64)
-		//fmt.Println("-----signature:" + signature)
-		request.SetHeader("Signature", signature)
+	for k, v := range headerMap {
+		request.SetHeader(k, v)
 	}
+
 	response := &protocol.Response{}
 	err = c.Do(context.Background(), request, response)
 	if err != nil {
@@ -130,5 +107,56 @@ func sendActivityPubRequest(httpurl string, method string, body map[string]inter
 	json.Unmarshal(response.Body(), &bodyMap)
 
 	return bodyMap, nil
+
+}
+
+func wrapRequestHeader(httpurl string, method string, body map[string]interface{}, keyId string, isSign bool) (map[string]string, error) {
+	headerMap := make(map[string]string)
+	// 解析 URL 字符串
+	parsedURL, err := url.Parse(httpurl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL:%w", err)
+	}
+
+	// mastodon 的日期格式为RFC2616 -- time.RFC1123
+	date := time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05 GMT")
+
+	bodyByte := []byte("")
+	if body != nil {
+		bodyByte, _ = json.Marshal(body)
+	}
+	hash := sha256.Sum256(bodyByte)
+	digest := "SHA-256=" + base64.StdEncoding.EncodeToString(hash[:])
+
+	headerMap["Digest"] = digest
+	headerMap["Date"] = date
+
+	method = strings.ToLower(method)
+	if method == "post" {
+		isSign = true
+	}
+	if isSign {
+		headers := "(request-target) date digest content-type"
+		comparisonStrings := make([]string, 0)
+		comparisonStrings = append(comparisonStrings, "(request-target): "+method+" "+parsedURL.Path)
+		//先不要host,避免转发服务器验签失败,可以转发到其他服务器
+		//comparisonStrings = append(comparisonStrings, "host: "+host)
+		comparisonStrings = append(comparisonStrings, "date: "+date)
+		comparisonStrings = append(comparisonStrings, "digest: "+digest)
+		comparisonStrings = append(comparisonStrings, "content-type: "+activityPubContentType)
+		signatureString := strings.Join(comparisonStrings, "\n")
+
+		//生成签名数据
+		signatureBase64, err := generateRSASignature(signatureString)
+		if err != nil {
+			return nil, err
+		}
+		// 构建 signature
+		signature := fmt.Sprintf(`keyId="%s",algorithm="rsa-sha256",headers="%s",signature="%s"`, keyId, headers, signatureBase64)
+		//fmt.Println("-----signature:" + signature)
+		headerMap["Signature"] = signature
+	}
+
+	return headerMap, nil
 
 }
