f8a0daa302ab224df4249df06f359ad3a844e12c
panpan <994776685@qq.com>
2023-05-24 19:09:24 +0800

    增加下拉菜单组件，封面上传对接

--- gpressdatadir/template/admin/js/dropDown.js
+++ gpressdatadir/template/admin/js/dropDown.js
@@ -36,9 +36,9 @@ DropDown.prototype = {
 			$.each(list, function(i, item) {
 				if (curr == item.id) {
 					liHtml +=
-						`<li id="${item.id}" name="${item.menuName}" class="aa active">${item.menuName}</li>`
+						`<li id="${item.id}" name="${item.menuName}" class="active">${item.menuName}</li>`
 				} else {
-					liHtml += `<li id="${item.id}" name="${item.menuName}" class="aa">${item.menuName}</li>`
+					liHtml += `<li id="${item.id}" name="${item.menuName}">${item.menuName}</li>`
 				}
 			})
 			$('#dropdown-list').append(liHtml)
