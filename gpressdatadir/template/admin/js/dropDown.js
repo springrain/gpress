a9c5cba0ce017983a35c10b6a3b1c845bb4f7f66
panpan <994776685@qq.com>
2023-05-24 18:26:34 +0800

    提交

---
+++ gpressdatadir/template/admin/js/dropDown.js
@@ -0,0 +1,61 @@
+// 下拉菜单选封装
+function DropDown(settings){
+	this.settings = settings
+	this.init()
+}
+
+DropDown.prototype = {
+	init: function() {
+		this.create()
+	},
+	create: function(){
+		$('.dropdown-box').remove()
+		let _template = `
+			<div class="dropdown-box">
+				<div class="dropdown-val button">
+					<input class="input-c input" type="text" value="${this.settings.default}" readonly
+						placeholder="请选择" id="navMenuName">
+					<input type="hidden" id="currval" value="${this.settings.id}" />
+					<i class="iconfont icon-tubiao-"></i>
+				</div>
+				<ul class="dropdown-list" id="dropdown-list">
+					<!-- 下拉列表 -->
+				</ul>
+			</div>
+		`
+		$(this.settings.container).append(_template)
+		this.refreshDom(this.settings)
+	},
+	refreshDom: function(){
+		var _this = this
+		$('.dropdown-val').click(function() {
+			let list = _this.settings.data
+			let liHtml = ''
+			let curr = $('#currval').val()
+			$('#dropdown-list li').remove()
+			$.each(list, function(i, item) {
+				if (curr == item.id) {
+					liHtml +=
+						`<li id="${item.id}" name="${item.menuName}" class="aa active">${item.menuName}</li>`
+				} else {
+					liHtml += `<li id="${item.id}" name="${item.menuName}" class="aa">${item.menuName}</li>`
+				}
+			})
+			$('#dropdown-list').append(liHtml)
+			$('#dropdown-list').toggle()
+		})
+		
+		$('#dropdown-list').on('click', 'li', function() {
+			let curr = $(this).attr('id')
+			_this.settings.default = $(this).attr('name')
+			_this.settings.id = curr
+			_this.settings.callBack({ 'id':curr,'name':$(this).attr('name') })
+			$('#dropdown-list').hide()
+			_this.create()
+		})
+	},
+}
+var dropDownInit = function(opts) {
+	return new DropDown(opts)
+}
+window.dropDownInit = $.dropDownInit = dropDownInit
