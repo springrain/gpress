9b4bf374f2b2fb6d3cea733d8cf8038d3e5ca987
springrain <chunanyong@163.com>
2023-06-01 10:08:03 +0800

    去掉分词器类型字段

--- initTableField.go
+++ initTableField.go
@@ -17,7 +17,7 @@ type TableFieldStruct struct {
 	ID string `column:"id" json:"id"`
 	// TableCode 表代码,类似表名 User,Site,PageTemplate,Category,Module,Content
 	TableCode string `column:"tableCode" json:"tableCode,omitempty"`
-	// TableName 表表中文名
+	// TableName 表中文名
 	TableName string `column:"tableName" json:"tableName,omitempty"`
 	// BusinessID  业务ID,处理业务记录临时增加的字段,意外情况
 	BusinessID string `column:"businessID" json:"businessID,omitempty"`
@@ -38,7 +38,7 @@ type TableFieldStruct struct {
 	//SelectOption 下拉框的选项值
 	SelectOption string `column:"selectOption" json:"selectOption,omitempty"`
 	// AnalyzerName  分词器名称
-	AnalyzerName string `column:"analyzerName" json:"analyzerName,omitempty"`
+	// AnalyzerName string `column:"analyzerName" json:"analyzerName,omitempty"`
 	// CreateTime 创建时间
 	CreateTime string `column:"createTime" json:"createTime,omitempty"`
 	// UpdateTime 更新时间
@@ -84,7 +84,6 @@ func initTableField() (bool, error) {
 		required          INT ,
 		defaultValue      TEXT,
 		selectOption      TEXT,
-		analyzerName      TEXT,
 		createTime        TEXT,
 		updateTime        TEXT,
 		createUser        TEXT,
@@ -100,12 +99,12 @@ func initTableField() (bool, error) {
 	sortNo := 1
 	now := time.Now().Format("2006-01-02 15:04:05")
 	id := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "id",
-		FieldName:    "字段ID",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "id",
+		FieldName: "字段ID",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: keyword// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -121,12 +120,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	tableCode := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "tableCode",
-		FieldName:    "表名",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "tableCode",
+		FieldName: "表名",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: keyword// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -142,12 +141,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	tableName := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "tableName",
-		FieldName:    "表名",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: gseAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "tableName",
+		FieldName: "表名",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: gse// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -163,12 +162,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	businessID := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "businessID",
-		FieldName:    "业务ID",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "businessID",
+		FieldName: "业务ID",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: keyword// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -184,12 +183,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	fieldCode := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "fieldCode",
-		FieldName:    "字段",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "fieldCode",
+		FieldName: "字段",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: keyword// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -205,12 +204,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	fieldName := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "fieldName",
-		FieldName:    "字段名",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: gseAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "fieldName",
+		FieldName: "字段名",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: gse// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -226,12 +225,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	fieldComment := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "fieldComment",
-		FieldName:    "字段备注",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: gseAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "fieldComment",
+		FieldName: "字段备注",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: gse// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -247,12 +246,12 @@ func initTableField() (bool, error) {
 	sortNo++
 	//ftSelect, _ := json.Marshal(fieldTypeMap)
 	fieldType := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "fieldType",
-		FieldName:    "字段类型",
-		FieldType:    fieldType_数字,
-		AnalyzerName: numericAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "fieldType",
+		FieldName: "字段类型",
+		FieldType: fieldType_数字,
+		// AnalyzerName: numeric// AnalyzerName,
 		//DefaultValue: "3",
 		// FieldType  字段类型,数字(1),日期(2),文本框(3),文本域(4),富文本(5),下拉框(6),单选(7),多选(8),上传图片(9),上传附件(10),轮播图(11),音频(12),视频(13)
 		//SelectOption: `[{"text":"数字","value":1},{"text":"日期","value":2},{"text":"文本框","value":3},{"text":"文本域","value":4},{"text":"富文本","value":5},{"text":"下拉框","value":6},{"text":"单选","value":7},{"text":"多选","value":8},{"text":"上传图片","value":9},{"text":"上传附件","value":10},{"text":"轮播图","value":11},{"text":"音频","value":12},{"text":"视频","value":13}]`,
@@ -272,12 +271,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	fieldFormat := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "fieldFormat",
-		FieldName:    "数据格式",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "fieldFormat",
+		FieldName: "数据格式",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: keyword// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -293,12 +292,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	required := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "required",
-		FieldName:    "是否必填",
-		FieldType:    fieldType_数字,
-		AnalyzerName: numericAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "required",
+		FieldName: "是否必填",
+		FieldType: fieldType_数字,
+		// AnalyzerName: numeric// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -314,12 +313,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	defaultValue := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "defaultValue",
-		FieldName:    "默认值",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "defaultValue",
+		FieldName: "默认值",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: keyword// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -334,34 +333,35 @@ func initTableField() (bool, error) {
 	//bleveSaveTable(indexFieldName, defaultValue.ID, defaultValue)
 	sortNo++
 
-	analyzerName := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "analyzerName",
-		FieldName:    "分词器",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
-		TableName:    "表字段",
-		FieldComment: "",
-		CreateTime:   now,
-		UpdateTime:   now,
-		CreateUser:   createUser,
-		SortNo:       sortNo,
-		Status:       3,
-		Required:     1,
-	}
-	saveTableField(ctx, analyzerName)
-	//bleveSaveTable
-	//bleveSaveTable(indexFieldName, analyzerName.ID, analyzerName)
-	sortNo++
-
+	/*
+		analyzerName := TableFieldStruct{
+			ID:        FuncGenerateStringID(),
+			TableCode: tableFieldName,
+			FieldCode: "analyzerName",
+			FieldName: "分词器",
+			FieldType: fieldType_文本框,
+			// AnalyzerName: keyword// AnalyzerName,
+			TableName:    "表字段",
+			FieldComment: "",
+			CreateTime:   now,
+			UpdateTime:   now,
+			CreateUser:   createUser,
+			SortNo:       sortNo,
+			Status:       3,
+			Required:     1,
+		}
+		saveTableField(ctx, analyzerName)
+		//bleveSaveTable
+		//bleveSaveTable(indexFieldName, analyzerName.ID, analyzerName)
+		sortNo++
+	*/
 	createTime := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "createTime",
-		FieldName:    "创建时间",
-		FieldType:    fieldType_日期,
-		AnalyzerName: datetimeAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "createTime",
+		FieldName: "创建时间",
+		FieldType: fieldType_日期,
+		// AnalyzerName: datetime// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -377,12 +377,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	updateTime := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "updateTime",
-		FieldName:    "更新时间",
-		FieldType:    fieldType_日期,
-		AnalyzerName: datetimeAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "updateTime",
+		FieldName: "更新时间",
+		FieldType: fieldType_日期,
+		// AnalyzerName: datetime// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -398,12 +398,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	createUserField := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "createUser",
-		FieldName:    "创建人",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "createUser",
+		FieldName: "创建人",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: keyword// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -419,12 +419,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	sortNoField := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "sortNo",
-		FieldName:    "排序",
-		FieldType:    fieldType_数字,
-		AnalyzerName: numericAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "sortNo",
+		FieldName: "排序",
+		FieldType: fieldType_数字,
+		// AnalyzerName: numeric// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -440,12 +440,12 @@ func initTableField() (bool, error) {
 	sortNo++
 
 	status := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableFieldName,
-		FieldCode:    "status",
-		FieldName:    "是否有效",
-		FieldType:    fieldType_数字,
-		AnalyzerName: numericAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableFieldName,
+		FieldCode: "status",
+		FieldName: "是否有效",
+		FieldType: fieldType_数字,
+		// AnalyzerName: numeric// AnalyzerName,
 		TableName:    "表字段",
 		FieldComment: "",
 		CreateTime:   now,
@@ -466,12 +466,12 @@ func initTableField() (bool, error) {
 func indexCommonField(ctx context.Context, tableCode string, tableName string, sortNo int, now string) {
 	sortNo++
 	commonCreateTime := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableCode,
-		FieldCode:    "createTime",
-		FieldName:    "创建时间",
-		FieldType:    fieldType_日期,
-		AnalyzerName: datetimeAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableCode,
+		FieldCode: "createTime",
+		FieldName: "创建时间",
+		FieldType: fieldType_日期,
+		// AnalyzerName: datetime// AnalyzerName,
 		TableName:    tableName,
 		FieldComment: "",
 		CreateTime:   now,
@@ -485,12 +485,12 @@ func indexCommonField(ctx context.Context, tableCode string, tableName string, s
 
 	sortNo++
 	commonUpdateTime := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableCode,
-		FieldCode:    "updateTime",
-		FieldName:    "更新时间",
-		FieldType:    fieldType_日期,
-		AnalyzerName: datetimeAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableCode,
+		FieldCode: "updateTime",
+		FieldName: "更新时间",
+		FieldType: fieldType_日期,
+		// AnalyzerName: datetime// AnalyzerName,
 		TableName:    tableName,
 		FieldComment: "",
 		CreateTime:   now,
@@ -504,12 +504,12 @@ func indexCommonField(ctx context.Context, tableCode string, tableName string, s
 
 	sortNo++
 	commonCreateUser := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableCode,
-		FieldCode:    "createUser",
-		FieldName:    "创建人",
-		FieldType:    fieldType_文本框,
-		AnalyzerName: keywordAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableCode,
+		FieldCode: "createUser",
+		FieldName: "创建人",
+		FieldType: fieldType_文本框,
+		// AnalyzerName: keyword// AnalyzerName,
 		TableName:    tableName,
 		FieldComment: "",
 		CreateTime:   now,
@@ -523,12 +523,12 @@ func indexCommonField(ctx context.Context, tableCode string, tableName string, s
 
 	sortNo++
 	commonSortNo := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableCode,
-		FieldCode:    "sortNo",
-		FieldName:    "排序",
-		FieldType:    fieldType_数字,
-		AnalyzerName: numericAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableCode,
+		FieldCode: "sortNo",
+		FieldName: "排序",
+		FieldType: fieldType_数字,
+		// AnalyzerName: numeric// AnalyzerName,
 		TableName:    tableName,
 		FieldComment: "",
 		CreateTime:   now,
@@ -542,12 +542,12 @@ func indexCommonField(ctx context.Context, tableCode string, tableName string, s
 
 	sortNo++
 	commonActive := TableFieldStruct{
-		ID:           FuncGenerateStringID(),
-		TableCode:    tableCode,
-		FieldCode:    "status",
-		FieldName:    "是否有效",
-		FieldType:    fieldType_数字,
-		AnalyzerName: numericAnalyzerName,
+		ID:        FuncGenerateStringID(),
+		TableCode: tableCode,
+		FieldCode: "status",
+		FieldName: "是否有效",
+		FieldType: fieldType_数字,
+		// AnalyzerName: numeric// AnalyzerName,
 		TableName:    tableName,
 		FieldComment: "",
 		CreateTime:   now,
