32f62cf9628021db8b3c1bb08371422308840f73
springrain <chunanyong@163.com>
2026-01-05 10:55:51 +0800

    增加升级SQL

--- const.go
+++ const.go
@@ -73,5 +73,5 @@ const (
 	compressedFileSuffix = ".gz"
 
 	//版本号
-	version = "v1.1.8"
+	version = "v1.1.9"
 )
---
+++ gpressdatadir/update_to_v1.1.9.sql
@@ -0,0 +1,102 @@
+-- 重命名 config 表的字段
+ALTER TABLE config RENAME COLUMN basePath TO base_path;
+ALTER TABLE config RENAME COLUMN jwtSecret TO jwt_secret;
+ALTER TABLE config RENAME COLUMN jwttokenKey TO jwt_token_key;
+ALTER TABLE config RENAME COLUMN serverPort TO server_port;
+ALTER TABLE config RENAME COLUMN maxRequestBodySize TO max_request_body_size;
+ALTER TABLE config RENAME COLUMN createTime TO create_time;
+ALTER TABLE config RENAME COLUMN updateTime TO update_time;
+ALTER TABLE config RENAME COLUMN createUser TO create_user;
+ALTER TABLE config RENAME COLUMN sortNo TO sortno;
+
+-- 重命名 user 表的字段(user是SQL关键字,需要用引号括起来)
+ALTER TABLE "user" RENAME COLUMN userName TO user_name;
+ALTER TABLE "user" RENAME COLUMN chainType TO chain_type;
+ALTER TABLE "user" RENAME COLUMN chainAddress TO chain_address;
+ALTER TABLE "user" RENAME COLUMN createTime TO create_time;
+ALTER TABLE "user" RENAME COLUMN updateTime TO update_time;
+ALTER TABLE "user" RENAME COLUMN createUser TO create_user;
+ALTER TABLE "user" RENAME COLUMN sortNo TO sortno;
+
+-- 重命名 category 表的字段
+ALTER TABLE category RENAME COLUMN hrefURL TO href_url;
+ALTER TABLE category RENAME COLUMN hrefTarget TO href_target;
+ALTER TABLE category RENAME COLUMN templateFile TO template_file;
+ALTER TABLE category RENAME COLUMN childTemplateFile TO child_template_file;
+ALTER TABLE category RENAME COLUMN createTime TO create_time;
+ALTER TABLE category RENAME COLUMN updateTime TO update_time;
+ALTER TABLE category RENAME COLUMN createUser TO create_user;
+ALTER TABLE category RENAME COLUMN sortNo TO sortno;
+
+-- 重命名 content 表的字段
+ALTER TABLE content RENAME COLUMN hrefURL TO href_url;
+ALTER TABLE content RENAME COLUMN categoryID TO category_id;
+ALTER TABLE content RENAME COLUMN categoryName TO category_name;
+ALTER TABLE content RENAME COLUMN templateFile TO template_file;
+ALTER TABLE content RENAME COLUMN signAddress TO sign_address;
+ALTER TABLE content RENAME COLUMN signChain TO sign_chain;
+ALTER TABLE content RENAME COLUMN txID TO tx_id;
+ALTER TABLE content RENAME COLUMN createTime TO create_time;
+ALTER TABLE content RENAME COLUMN updateTime TO update_time;
+ALTER TABLE content RENAME COLUMN createUser TO create_user;
+ALTER TABLE content RENAME COLUMN sortNo TO sortno;
+
+-- 重命名 site 表的字段
+ALTER TABLE site RENAME COLUMN themePC TO theme_pc;
+ALTER TABLE site RENAME COLUMN themeWAP TO theme_wap;
+ALTER TABLE site RENAME COLUMN themeWX TO theme_wx;
+ALTER TABLE site RENAME COLUMN createTime TO create_time;
+ALTER TABLE site RENAME COLUMN updateTime TO update_time;
+ALTER TABLE site RENAME COLUMN createUser TO create_user;
+ALTER TABLE site RENAME COLUMN sortNo TO sortno;
+
+
+-- 删除现有的触发器和虚拟表
+DROP TRIGGER IF EXISTS trigger_content_insert;
+DROP TRIGGER IF EXISTS trigger_content_delete;
+DROP TRIGGER IF EXISTS trigger_content_update;
+DROP TABLE IF EXISTS fts_content;
+
+
+-- 使用设置-->更新SQL 的功能,按照步骤执行以下SQL,每次执行一行
+
+-- 重新创建虚拟表(使用新的字段名)
+CREATE VIRTUAL TABLE IF NOT EXISTS fts_content USING fts5(
+    title, 
+    keyword, 
+    description,
+    subtitle,
+    category_name,  
+    summary,
+    toc,
+    tag,
+    author,
+    tokenize = 'simple 0',
+    content='content', 
+    content_rowid='rowid'
+);
+
+-- 重新创建触发器(使用新的字段名)
+CREATE TRIGGER IF NOT EXISTS trigger_content_insert AFTER INSERT ON content
+BEGIN
+    INSERT INTO fts_content (rowid, title, keyword, description, subtitle, category_name, summary, toc, tag, author)
+    VALUES (new.rowid, new.title, new.keyword, new.description, new.subtitle, new.category_name, new.summary, new.toc, new.tag, new.author);
+END;
+
+CREATE TRIGGER IF NOT EXISTS trigger_content_delete AFTER DELETE ON content
+BEGIN
+    INSERT INTO fts_content (fts_content, rowid, title, keyword, description, subtitle, category_name, summary, toc, tag, author)
+    VALUES ('delete', old.rowid, old.title, old.keyword, old.description, old.subtitle, old.category_name, old.summary, old.toc, old.tag, old.author);
+END;
+
+CREATE TRIGGER IF NOT EXISTS trigger_content_update AFTER UPDATE ON content
+BEGIN
+    INSERT INTO fts_content (fts_content, rowid, title, keyword, description, subtitle, category_name, summary, toc, tag, author)
+    VALUES ('delete', old.rowid, old.title, old.keyword, old.description, old.subtitle, old.category_name, old.summary, old.toc, old.tag, old.author);
+    INSERT INTO fts_content (rowid, title, keyword, description, subtitle, category_name, summary, toc, tag, author)
+    VALUES (new.rowid, new.title, new.keyword, new.description, new.subtitle, new.category_name, new.summary, new.toc, new.tag, new.author);
+END;
+
+
+INSERT INTO fts_content (rowid, title, keyword, description, subtitle, category_name, summary, toc, tag, author)
+SELECT rowid, title, keyword, description, subtitle, category_name, summary, toc, tag, author FROM content;
